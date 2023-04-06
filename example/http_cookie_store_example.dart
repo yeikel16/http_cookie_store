3aa8232a395ddcb1fe32d9950ec3336931196459
Paul Suckow <46897578+Dampfwalze@users.noreply.github.com>
2023-04-06 16:47:40 +0200

    Add: Example

--- example/http_cookie_store_example.dart
+++ example/http_cookie_store_example.dart
@@ -1,6 +1,21 @@
+import 'package:collection/collection.dart';
 import 'package:http_cookie_store/http_cookie_store.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+void main() async {
+  final client = CookieClient();
+
+  await client.get(Uri.parse('https://www.nytimes.com/'));
+
+  await client.get(Uri.parse('https://edition.cnn.com/'));
+
+  groupBy(client.store.cookies, (c) => c.domain).forEach((key, value) {
+    print("$key:");
+    for (var cookie in value) {
+      print('\t$cookie');
+    }
+  });
+
+  print(client.store[CookieKey('geoData', Uri(host: 'cnn.com'))]);
+
+  client.close();
 }
